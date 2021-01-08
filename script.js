6dca6860470cdfbb26018132cd114fa3a4907af7
adeha047 <dehaven7777@gmail.com>
2021-01-07 21:51:39 -0500

    created new readMe's and updated Script

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
+// Writes password to the #password input when requested
 function getRandom(arr) {
   var randomNumber = Math.floor(Math.randon() * arr.length);
   var characters = arr[randomNumber]
@@ -16,55 +16,104 @@ function getRandom(arr) {
 
   function generatePassword() {
    
-    // adding a new array to account for the pas
+    // accounting for passwords
    var password = "";
    var passwordType = [];
    //accounts for all the possible characters in the password
    var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
    var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
    var special = ["!", "@", "#", "$", "%", "^", "&", "*"];
-   var numb = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
- 
-
+   var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+   var allCharacters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "!", "@", "#", "$", "%", "^", "&", "*", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+   var specialNumbers = ["!", "@", "#", "$", "%", "^", "&", "*", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+   var lowUp = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+   var lowUpSpecial = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "!", "@", "#", "$", "%", "^", "&", "*"];
+   var lowNumbers = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+   var highNumbers = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+   var lowCharacters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z","@", "#", "$", "%", "^", "&", "*"];
+   var highCharacters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z","@", "#", "$", "%", "^", "&", "*"];
+    
+  
+    // Accounts for the length of the variables selected
    var length = prompt("How many characters would you like your Password to be?");
 
+   //Accounts for the length of the password
    if (length < 8 || length > 128) {
+     //only options between 8 and 128 are available
      alert("Please enter a number between 8 and 128")
      return;
 
    }
 
-
+   // Questions determining how the password should be formatted
    var lowercase = confirm("Do you want to use lowercase letters?");
    var uppercase = confirm("Do you want to use Upperrcase letters?");
    var numeric = confirm("Do you want to include numbers?");
    var specialCharacters = confirm("Do you want to include special characters?");
-
+   
+   // Must Choose at least one option
    if (lowercase === false && uppercase === false && numeric === false && specialCharacters === false) {
      alert("Please use one of the requested fields.")
    }
-   
+   //For if only lowercase is selected 
    if (lowercase === true && uppercase === false && numeric === false && specialCharacters === false) {
      passwordType = passwordType.concat(lowerCase);
 
    }
-
+   //for if only uppercase options are selected
    if (lowercase === false && uppercase === true && numeric === false && specialCharacters === false) {
     passwordType = passwordType.concat(upperCase);
 
    }
-
+   // For if only numbers are selected 
    if (lowercase === false && uppercase === false && numeric === true && specialCharacters === false) {
-    passwordType = passwordType.concat(numeric);
+    passwordType = passwordType.concat(numbers);
+
+   }
+   //For if only special characters are selected
+   if (lowercase === false && uppercase === false && numeric === false && specialCharacters === true) {
+    passwordType = passwordType.concat(special);
+
+   }
+   //For if all characters are selected
+   if (lowercase === true && uppercase === true && numeric === true && specialCharacters === true) {
+    passwordType = passwordType.concat(allCharacters); 
+
+   }
+   //For if numbers and special characters are selected.
+   if (lowercase === false && uppercase === false && numeric === true && specialCharacters === true) {
+    passwordType = passwordType.concat(specialNumbers); 
+
+   }
+   //For different variations that have been requested in the prompts 
+   if (lowercase === true && uppercase === true && numeric === false && specialCharacters === false) {
+    passwordType = passwordType.concat(lowUp); 
+
+   }
+   //For different variations that have been requested in the prompts 
+   if (lowercase === true && uppercase === true && numeric === true && specialCharacters === false) {
+    passwordType = passwordType.concat(lowUpSpecial); 
 
    }
 
-   if (lowercase === false && uppercase === false && numeric === true && specialCharacters === false) {
-    passwordType = passwordType.concat(specialCharacters);
+   //For different variations that have been requested in the prompts 
+    if (lowercase === true && uppercase === false && numeric === true && specialCharacters === false) {
+      passwordType = passwordType.concat(lowNumbers); 
+    }
 
-   }
+    if (lowercase === false && uppercase === true && numeric === true && specialCharacters === false) {
+      passwordType = passwordType.concat(highNumbers); 
+    }
 
+    if (lowercase === true && uppercase === false && numeric === false && specialCharacters === true) {
+      passwordType = passwordType.concat(lowCharacters); 
+    }
 
+    if (lowercase === false && uppercase === true && numeric === false && specialCharacters === true) {
+      passwordType = passwordType.concat(highCharacters); 
+    }
+
+   //For loop to determine the length and type of password
    var passwords = length; 
    for (var i = 0; i < passwords; i++) {
      var randomNumber = Math.floor(Math.random() * passwordType.length); 
